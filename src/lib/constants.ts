9e44e489b248e34c3ba03d71a882bd52a956273b
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-07-31 01:28:33 +0800

    chore: update directory names for drafts and archives in constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ import { join } from "path";
 const BASE_URL = "https://dev.1chooo.com";
 
 const POSTS_DIR = join(process.cwd(), "src", "content");
-const DRAFTS_DIR = join(process.cwd(), "src", "content", "drafts");
-const ARCHIVES_DIR = join(process.cwd(), "src", "content", "archives");
+const DRAFTS_DIR = join(process.cwd(), "src", "content", "draft");
+const ARCHIVES_DIR = join(process.cwd(), "src", "content", "archive");
 
 export { BASE_URL, POSTS_DIR, DRAFTS_DIR, ARCHIVES_DIR };
